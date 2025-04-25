6bbe65e1c170b9042aa4c442608b59b1ea0f2514
MostafSayed1 <mostafasayed7410@gmail.com>
2025-04-25 10:38:39 +0300

    usernav

---
+++ src/app/Layout/Pages/update-ban/update-ban.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-update-ban',
+  imports: [],
+  templateUrl: './update-ban.component.html',
+  styleUrl: './update-ban.component.css'
+})
+export class UpdateBanComponent {
+
+}
